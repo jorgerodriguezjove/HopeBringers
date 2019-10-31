316a08f19a71117207732908b69cb296fb004dfa
DavidCalvoSuarez <david.calvo@live.u-tad.com>
2019-10-31 13:15:19 +0100

    Animaciones, script y materiales relacionados con el libro. Importe de cinemachine

---
+++ Assets/Scripting/Menu/BookMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookMenu : MonoBehaviour
+{
+
+    public Animator anim;
+    // Start
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetBool("isOpen?", true);
+            
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            anim.SetBool("isOpen?", false);
+            
+           
+        }
+
+    }
+}
