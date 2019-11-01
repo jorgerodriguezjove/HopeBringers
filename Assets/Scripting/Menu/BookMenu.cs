3837d19f6a0baa2e0e649457141e0dcf195a51d8
DavidCalvoSuarez <david.calvo@live.u-tad.com>
2019-11-01 10:34:43 +0100

    Terminada la animación del dolly track del libro. Mirar con Mario o Jorge como administrar diferentes animaciones del Timeline o pasarlas al blend tree or whatever

--- Assets/Scripting/Menu/BookMenu.cs
+++ Assets/Scripting/Menu/BookMenu.cs
@@ -7,23 +7,25 @@ public class BookMenu : MonoBehaviour
 {
     public PlayableDirector DollyCamera;
     public Animator anim;
+    bool isOpen;
     // Start
     void Start()
     {
         anim = GetComponent<Animator>();
+        isOpen = anim.GetBool("isOpen?");
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)/* && isOpen*/)
         {
             anim.SetBool("isOpen?", true);
             DollyCamera.Play();
         }
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1)/* && !isOpen*/)
         {
             anim.SetBool("isOpen?", false);
-            
+            DollyCamera.Play();
            
         }
     }
