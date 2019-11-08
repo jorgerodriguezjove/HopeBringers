2d5cbbcd5ad3f9905895ac00d34d500fe7167788
DavidCalvoSuarez <david.calvo@live.u-tad.com>
2019-11-08 11:49:23 +0100

    Shaders de selección y unidades al alcanze. Falta hacer materiales copai de la base de polygon respecto a los diferentes efectos de shader.

---
+++ Assets/Scripting/DraggingTests/MeshHighlighter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SkinnedMeshRenderer))]
+public class MeshHighlighter : MonoBehaviour
+{
+
+    private SkinnedMeshRenderer originalMesh;
+    [SerializeField]
+    private SkinnedMeshRenderer highlightedMesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalMesh = GetComponent<SkinnedMeshRenderer>();
+        EnabledHighlight(false);
+    }
+
+    public void EnabledHighlight(bool onOff)
+    {
+        if(highlightedMesh != null)
+        {
+            highlightedMesh.enabled = onOff;
+            originalMesh.enabled = !onOff;
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        EnabledHighlight(true);
+    }
+
+    private void OnMouseExit()
+    {
+        EnabledHighlight(false);
+    }
+}
