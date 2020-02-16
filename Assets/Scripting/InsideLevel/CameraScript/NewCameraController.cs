fbc6b4d1754fab67d08d92adab3c4666540429b4
JorgeRodriguezJove <14obiwan@gmail.com>
2020-02-16 16:30:26 +0100

    Nueva cámara

--- Assets/Scripting/InsideLevel/CameraScript/NewCameraController.cs
+++ Assets/Scripting/InsideLevel/CameraScript/NewCameraController.cs
@@ -1,40 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class NewCameraController : MonoBehaviour
 {
     #region VARIABLES
 
-    ////Número de esquinas/posiciones de cámara
-    //int  numberOfCorners = 4;
+    [Header("POSICIÓN DE CÁMARA")]
 
-    //Distancia en x y z desde el centro del cuadrado hasta las posiciones de la cámara
+    //Distancia desde el focusPoint hasta la cámara.
     [SerializeField]
-    int squareSideLength;
-
-    ////Diágonal del cuadrado = Diámetro de circulo que inscribe al cuadrado. Se calcula en el start
-    //float diameter;
+    float distanceFromCenterToCamera;
 
     //Diferencia de altura entre las cámaras y el punto central.
     //Este número también implica la rotación de la cámara (lo picada que esta).
     [SerializeField]
-    int heightOffset;
+    float heightOffset;
 
+    //Distancia que se suma al hacer zoomOut
+    [SerializeField]
+    float zoomDifference;
 
-    [Header("Move Controls")]
+    [Header("CONTROLES DE CÁMARA")]
     [SerializeField]
     private float panSpeed;
     [SerializeField]
     private float borderThickness;
+    [SerializeField]
+    private float timeRotation;
+    [SerializeField]
+    private float timeZoom;
 
+    //Rotación de la cámara
     private enum cameraRotationPosition {north, east, south, west }
+    [SerializeField]
     private cameraRotationPosition currentRotationPosition;
 
+    private enum cameraZoomLevel {close, far }
+    private cameraZoomLevel currentCameraZoom;
+
+    //Vector que guarda la posición y los cambios que se realizan al pulsar las teclas para mover la cámara
     Vector3 pos;
 
-    ////Lista con las esquinas del cuadrado = posiciones de la cámara.
-    //List<Vector3> corners = new List<Vector3>();
+    //Quaternion en el que se guarda la rotación que tiene que usar según si está en north, east...
+    Quaternion referenceQuaternionToAddRotation;
+
+    //Bool que sirve para indicar si la cámara se puede mover y rotar. Se usa para no poder mover y rotar la cámara mientras se hace zoom;
+    [SerializeField]
+    bool canMoveAndRotateCamera = true;
+    //Bool que sirve para indicar si la cámara puede hacer zoom. Se usa para no poder hacer zoom mientras se está rotando la cámara.
+    [SerializeField]
+    bool canZoomCamera = true;
+
+    [Header("REFERENCIAS")]
 
     [SerializeField]
     GameObject myCamera;
@@ -43,6 +62,10 @@ public class NewCameraController : MonoBehaviour
     CameraBounds cameraBoundsRef;
     Vector3 cameraBoundsSize;
 
+    [Header("DEBUG")]
+    [SerializeField]
+    bool deactivateBorderMovement;
+
     #endregion
 
     private void Start()
@@ -50,40 +73,21 @@ public class NewCameraController : MonoBehaviour
         cameraBoundsSize = cameraBoundsRef.boundsSize;
 
         //Inicializo el punto central y el diámetro
-        myCamera.transform.position = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y + heightOffset, gameObject.transform.position.z - squareSideLength);
-        //diameter = squareSideLength * Mathf.Sqrt(2);
+        StartCoroutine("SetZoomCoroutine");
 
         currentRotationPosition = cameraRotationPosition.north;
 
-        ////Creo el cuadrado
-        //CreateSquare();
+        currentCameraZoom = cameraZoomLevel.close;
     }
 
+    #region ROTATION
 
-
-    private void CreateSquare()
+    IEnumerator RotateCameraRightCoroutine()
     {
-        ////Cámara abajo que mira hacia el norte
-        //corners.Add(new Vector3(centerPoint.position.x, centerPoint.position.y + heightOffset, centerPoint.position.z - squareSideLength));
+        canZoomCamera = false;
 
-        ////Cámara izquierda que mira hacia el este
-        //corners.Add(new Vector3(centerPoint.position.x - squareSideLength, centerPoint.position.y + heightOffset, centerPoint.position.z));
-
-        ////Cámara arriba que mira hacia el sur
-        //corners.Add(new Vector3(centerPoint.position.x, centerPoint.position.y + heightOffset, centerPoint.position.z + squareSideLength));
-
-        ////Cámara derecha que mira hacia el oeste
-        //corners.Add(new Vector3(centerPoint.position.x + squareSideLength, centerPoint.position.y + heightOffset, centerPoint.position.z ));
-
-        //for (int i = 0; i < corners.Count; i++)
-        //{
-        //    Instantiate(new GameObject(), corners[i], centerPoint.transform.rotation, centerPoint);
-        //}
-    }
-
-    public void RotateCameraRight()
-    {
-        myCamera.transform.RotateAround(gameObject.transform.position, Vector3.up, 90);
+        CalculateReferenceRotation();
+        gameObject.transform.DORotate((referenceQuaternionToAddRotation * Quaternion.Euler(0, 90, 0)).eulerAngles, timeRotation);
 
         if ((int)currentRotationPosition > 2)
         {
@@ -94,11 +98,18 @@ public class NewCameraController : MonoBehaviour
         {
             currentRotationPosition += 1;
         }
+
+        yield return new WaitForSeconds(timeRotation);
+
+        canZoomCamera = true;
     }
 
-    public void RotateCameraLeft()
+    IEnumerator RotateCameraLeftCoroutine()
     {
-        myCamera.transform.RotateAround(gameObject.transform.position, Vector3.down, 90);
+        canZoomCamera = false;
+
+        CalculateReferenceRotation();
+        gameObject.transform.DORotate((referenceQuaternionToAddRotation * Quaternion.Euler(0, -90, 0)).eulerAngles, timeRotation);
 
         if ((int)currentRotationPosition < 1)
         {
@@ -109,91 +120,190 @@ public class NewCameraController : MonoBehaviour
         {
             currentRotationPosition -= 1;
         }
+
+        yield return new WaitForSeconds(timeRotation);
+
+        canZoomCamera = true;
     }
+
+    //Calcula el transform.rotation que tendría el gameObject en cada punto (norte,sur...) para usarlo como referencia y sumarle o restarle 90º.
+    //Esto hace que en caso de darle varias veces al botón de rotar no tome la rotación del objeto mientras esta griando si no directametne en la que va a acabar.
+    private void CalculateReferenceRotation()
+    {
+        if (currentRotationPosition == cameraRotationPosition.north)
+            referenceQuaternionToAddRotation = new Quaternion(0,0,0,1);
+
+        if (currentRotationPosition == cameraRotationPosition.east)
+            referenceQuaternionToAddRotation = new Quaternion(0, 0.7f, 0, 0.7f);
+
+        if (currentRotationPosition == cameraRotationPosition.south)
+            referenceQuaternionToAddRotation = new Quaternion(0, 1, 0, 0);
+
+        if (currentRotationPosition == cameraRotationPosition.west)
+            referenceQuaternionToAddRotation = new Quaternion(0,0.7f, 0, -0.7f);
+    }
+    #endregion
+
+    #region ZOOM
+
+    private void ZoomOut()
+    {
+        if (currentCameraZoom != cameraZoomLevel.far)
+        {
+            distanceFromCenterToCamera += zoomDifference;
+            heightOffset += zoomDifference;
+            StartCoroutine("SetZoomCoroutine");
+            currentCameraZoom = cameraZoomLevel.far;
+        }
+    }
+
+    private void ZoomIn()
+    {
+        if (currentCameraZoom != cameraZoomLevel.close)
+        {
+            distanceFromCenterToCamera -= zoomDifference;
+            heightOffset -= zoomDifference;
+            StartCoroutine("SetZoomCoroutine");
+            currentCameraZoom = cameraZoomLevel.close;
+        }
+    }
+
+    IEnumerator SetZoomCoroutine()
+    {
+        canMoveAndRotateCamera = false;
+
+        if (currentRotationPosition == cameraRotationPosition.north)
+        {
+            myCamera.transform.DOMove(new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + heightOffset, gameObject.transform.position.z - distanceFromCenterToCamera), timeZoom);
+        }
+
+        else if (currentRotationPosition == cameraRotationPosition.east)
+        {
+            myCamera.transform.DOMove(new Vector3(gameObject.transform.position.x - distanceFromCenterToCamera, gameObject.transform.position.y + heightOffset, gameObject.transform.position.z), timeZoom);
+        }
+
+        else if (currentRotationPosition == cameraRotationPosition.south)
+        {
+            myCamera.transform.DOMove(new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + heightOffset, gameObject.transform.position.z + distanceFromCenterToCamera), timeZoom);
+        }
+
+        else if (currentRotationPosition == cameraRotationPosition.west)
+        {
+            myCamera.transform.DOMove(new Vector3(gameObject.transform.position.x + distanceFromCenterToCamera, gameObject.transform.position.y + heightOffset, gameObject.transform.position.z), timeZoom);
+        }
+
+        yield return new WaitForSeconds(timeZoom);
+
+        canMoveAndRotateCamera = true;
+    }
+
+    #endregion
 
     private void Update()
     {
-        Debug.Log((int)currentRotationPosition);
-
+        //La cámara siempre mira hacia el focusPoint
         myCamera.transform.LookAt(gameObject.transform);
 
+        //Seteo la posición actual
         pos = transform.position;
 
-        //Movimiento
-        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - borderThickness)
-        {
-            if (currentRotationPosition == cameraRotationPosition.north)
-                pos.z += panSpeed * Time.deltaTime;
-            if (currentRotationPosition == cameraRotationPosition.east)
-                pos.x += panSpeed * Time.deltaTime;
-            if (currentRotationPosition == cameraRotationPosition.south)
-                pos.z -= panSpeed * Time.deltaTime;
-            if (currentRotationPosition == cameraRotationPosition.west)
-                pos.x -= panSpeed * Time.deltaTime;
-        }
+        //Ifs que hacen que la cámara se mueva correctamente independientemente de la rotación de la cámara y modifican la posición.
+        #region MOVEMENT
 
-        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - borderThickness)
+        if (canMoveAndRotateCamera)
         {
-            if (currentRotationPosition == cameraRotationPosition.north)
-                pos.x += panSpeed * Time.deltaTime;
-            if (currentRotationPosition == cameraRotationPosition.east)
-                pos.z -= panSpeed * Time.deltaTime;
-            if (currentRotationPosition == cameraRotationPosition.south)
-                pos.x -= panSpeed * Time.deltaTime;
-            if (currentRotationPosition == cameraRotationPosition.west)
-                pos.z += panSpeed * Time.deltaTime;
-        }
+            if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - borderThickness && !deactivateBorderMovement)
+            {
+                if (currentRotationPosition == cameraRotationPosition.north)
+                    pos.z += panSpeed * Time.deltaTime;
+                if (currentRotationPosition == cameraRotationPosition.east)
+                    pos.x += panSpeed * Time.deltaTime;
+                if (currentRotationPosition == cameraRotationPosition.south)
+                    pos.z -= panSpeed * Time.deltaTime;
+                if (currentRotationPosition == cameraRotationPosition.west)
+                    pos.x -= panSpeed * Time.deltaTime;
+            }
 
-        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <=  borderThickness)
-        {
-            if (currentRotationPosition == cameraRotationPosition.north)
-                pos.z -= panSpeed * Time.deltaTime;
-            if (currentRotationPosition == cameraRotationPosition.east)
-                pos.x -= panSpeed * Time.deltaTime;
-            if (currentRotationPosition == cameraRotationPosition.south)
-                pos.z += panSpeed * Time.deltaTime;
-            if (currentRotationPosition == cameraRotationPosition.west)
-                pos.x += panSpeed * Time.deltaTime;
+            if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - borderThickness && !deactivateBorderMovement)
+            {
+                if (currentRotationPosition == cameraRotationPosition.north)
+                    pos.x += panSpeed * Time.deltaTime;
+                if (currentRotationPosition == cameraRotationPosition.east)
+                    pos.z -= panSpeed * Time.deltaTime;
+                if (currentRotationPosition == cameraRotationPosition.south)
+                    pos.x -= panSpeed * Time.deltaTime;
+                if (currentRotationPosition == cameraRotationPosition.west)
+                    pos.z += panSpeed * Time.deltaTime;
+            }
 
+            if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= borderThickness && !deactivateBorderMovement)
+            {
+                if (currentRotationPosition == cameraRotationPosition.north)
+                    pos.z -= panSpeed * Time.deltaTime;
+                if (currentRotationPosition == cameraRotationPosition.east)
+                    pos.x -= panSpeed * Time.deltaTime;
+                if (currentRotationPosition == cameraRotationPosition.south)
+                    pos.z += panSpeed * Time.deltaTime;
+                if (currentRotationPosition == cameraRotationPosition.west)
+                    pos.x += panSpeed * Time.deltaTime;
+
+            }
+
+            if (Input.GetKey(KeyCode.A) || Input.mousePosition.x <= borderThickness && !deactivateBorderMovement)
+            {
+                if (currentRotationPosition == cameraRotationPosition.north)
+                    pos.x -= panSpeed * Time.deltaTime;
+                if (currentRotationPosition == cameraRotationPosition.east)
+                    pos.z += panSpeed * Time.deltaTime;
+                if (currentRotationPosition == cameraRotationPosition.south)
+                    pos.x += panSpeed * Time.deltaTime;
+                if (currentRotationPosition == cameraRotationPosition.west)
+                    pos.z -= panSpeed * Time.deltaTime;
+            }
         }
-       
-        if (Input.GetKey(KeyCode.A) || Input.mousePosition.x <= borderThickness)
-        {
-            if (currentRotationPosition == cameraRotationPosition.north)
-                pos.x -= panSpeed * Time.deltaTime;
-            if (currentRotationPosition == cameraRotationPosition.east)
-                pos.z += panSpeed * Time.deltaTime;
-            if (currentRotationPosition == cameraRotationPosition.south)
-                pos.x += panSpeed * Time.deltaTime;
-            if (currentRotationPosition == cameraRotationPosition.west)
-                pos.z -= panSpeed * Time.deltaTime;
-        }
-       
+        #endregion
+
+        //Actualiza la posición
         transform.position = pos;
 
-
-
-
-
-
-        if (Input.GetKeyDown(KeyCode.Q))
+        //Input Rotación
+        if (canMoveAndRotateCamera)
         {
-            RotateCameraRight();
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                StopCoroutine("RotateCameraRightCoroutine");
+                StartCoroutine("RotateCameraRightCoroutine");
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                StopCoroutine("RotateCameraLeftCoroutine");
+                StartCoroutine("RotateCameraLeftCoroutine");
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.E))
+        //Input Zoom
+        if (canZoomCamera)
         {
-            RotateCameraLeft();
+            //Input Zoom
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                ZoomIn();
+            }
+
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                ZoomOut();
+            }
         }
     }
 
     private void LateUpdate()
     {
-
         LockPositionInBounds();
     }
 
-
+    //Mantengo el focuspoint dentro del área de juego
     private void LockPositionInBounds()
     {
         transform.position = new Vector3(
