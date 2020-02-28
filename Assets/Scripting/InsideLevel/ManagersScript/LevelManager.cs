0809001aa834b6f8d3ed2e75db86d8f86353cc0e
JorgeRodriguezJove <14obiwan@gmail.com>
2020-02-28 12:12:42 +0100

    Cambio de nivel, bug level 5 y tiles mal puestos en el 4

--- Assets/Scripting/InsideLevel/ManagersScript/LevelManager.cs
+++ Assets/Scripting/InsideLevel/ManagersScript/LevelManager.cs
@@ -551,8 +551,7 @@ public class LevelManager : MonoBehaviour
                                 }
                             }
                         }
-                    }
-                   
+                    }  
                 }
             }
         }
