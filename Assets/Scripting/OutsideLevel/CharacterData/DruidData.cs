7281b84387d8dd51c5172d7fef20b373788079d7
MarioGarciaGD <mariogarciagd@gmail.com>
2020-03-21 08:38:23 +0100

    Implementado niveles por el mapa He desactivado los personajes de los niveles y añadido spawn tiles (he quitado el tick del level manager)

---
+++ Assets/Scripting/OutsideLevel/CharacterData/DruidData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DruidData : CharacterData
+{
+
+    public override void UpdateMyUnitStatsForTheLevel()
+    {
+        //Referencia al personaje en el nivel
+        myUnitReferenceOnLevel = FindObjectOfType<Druid>();
+
+        if (myUnitReferenceOnLevel != null)
+        {
+            //Aztualizo las mejoras genéricas
+            base.UpdateMyUnitStatsForTheLevel();
+
+            //Actualizo las merjoas especificas del personaje
+            //HAY QUE CAMBIARLO EN SU SCRIPT
+            //myUnitReferenceOnLevel.GetComponent<Druid>().SetSpecificStats(specificBoolCharacterUpgrades[AppKnightUpgrades.pushFurther1], specificBoolCharacterUpgrades[AppKnightUpgrades.pushWider1]);
+        }
+    }
+
+    //Esto se llama en el INIT del characterData (padre de este script)
+    protected override void InitializeSpecificUpgrades()
+    {
+        //Mejoras Tipo BOOL
+        //HAY QUE CAMBIARLO EN SU SCRIPT
+        //specificBoolCharacterUpgrades.Add(AppKnightUpgrades.pushFurther1, false);
+        //specificBoolCharacterUpgrades.Add(AppKnightUpgrades.pushWider1, false);
+
+        //Mejoras tipo INT
+        //specificIntCharacterUpgrades.Add(AppKnightUpgrades.pushFurther1, myUnitReferenceOnLevel.GetComponent<Knight>().tilesToPush);
+    }
+
+}
+
